b6f3f44ae76cb9309e87f03238bb62c2ac4a3c76
vahiiiid <v.vahedi@dev.genom-co.ir>
2019-04-22 09:34:44 +0430

    index.php home url bug

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
           integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 
-    <title>tests!</title>
+    <title>pdp!</title>
 </head>
 <body>
 <nav class="navbar bg-dark navbar-dark navbar-expand-sm">
@@ -17,7 +17,7 @@
     <!-- Links -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link home active" href="http://localhost:8000/">Home</a>
+            <a class="nav-link home active" href="/">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link php" href="/?pattern=php">Php</a>
@@ -123,4 +123,3 @@ $app = App::getInstance();
 $app->dispatch($_GET['pattern']);
 
 ?>
-
